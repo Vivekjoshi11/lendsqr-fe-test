aa7186595a9665ff3c67976fc1133aa0405d0489
Vivekjoshi11 <108278744+Vivekjoshi11@users.noreply.github.com>
2025-02-21 00:18:30 +0530

    changes

--- src/components/layout/Dashboard.tsx
+++ src/components/layout/Dashboard.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import UserStats from "../users/UserStats";
-// import UserTable from "../users/UserTable";
 import "../styles/dashboard.scss";
 
 const Dashboard: React.FC = () => {
   return (
     <div className="dashboard">
       <UserStats />
-      {/* <UserTable /> */}
     </div>
   );
 };
